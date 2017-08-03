6f7ce8b2d0c50b53c19b48ab33f2b50d8837c005
proxy-hatch <wyx_100@hotmail.com>
2017-08-03 14:50:26 -0700

    release_V1

--- main.c
+++ main.c
@@ -33,7 +33,6 @@
 #include <dirent.h>     //opendir(), readdir(), struct dirent, etc.
 #include <sys/types.h>  // ino_t
 #include <errno.h>      // errno
-#include <sys/stat.h>   // stat
 #include <time.h>       // localtime_r()
 #include <stdlib.h>     // malloc() free()
 #include <pwd.h>        // getpwuid()
@@ -42,7 +41,7 @@
 
 // DEBUG macro is used to turn on various debugging features
 // Disable at the release version
-#define DEBUG
+//#define DEBUG
 
 /* POSIX.1 says each process has at least 20 file descriptors.
  * Three of those belong to the standard streams.
@@ -71,6 +70,8 @@ static char currDir[BUFFSIZE];
 static int iFlag;
 static int RFlag;
 static int lFlag;
+// for cases when there is 1 or less dir path arguments passed in. 'ls' cmd does not print the header
+static int omitHeaderFlag;
 
 // Finds a specific string in a (portion of) str array
 // used for finding special patterns '..', '.', '~' in argv[]
@@ -82,7 +83,7 @@ static int findArgument(char *pattern, char **strArr, int beginArrPos, int endAr
 
 // prints the directory passed in
 // return errno (0= no error)
-int print_directory_tree(const char *const dirpath);
+int print_directory_tree(const char *dirpath);
 
 // core function that prints the  info in a file/directory given
 // parameters specified by nftw()
@@ -98,13 +99,15 @@ char const *sperm(__mode_t mode);
 // Boolean func to determine whether the filepath is a special directory
 // special directory is defined as one of the three: '.', '..', or a hidden file (name beginning with .xx)
 // filename must not contain path (including ./xxx)
-// return 1 for true; 0 for false
+// return 1 for "." or ".."; return 2 for hidden file/dir; 0 for false
 int isSpecialDir(const char *filename);
 
+#ifdef DEBUG
 // determines if the first x chars matches currDir, if it does, we can assume that its a subdirectory
 // handles filepaths in the format of both './xxx' and plain 'xxx'
-// returns non-zero for true, 0 for false
-int isSubDir(const char *filePath);
+// returns ptr to the location of '/' for true, 0 for false
+char *isSubDir(const char *filePath);
+#endif
 
 // this function trims the filepath
 // similar to basename(), but substitutes the filename to '.' if it matches currDir[]
@@ -139,26 +142,42 @@ int main(int argc, char *argv[]) {
         }
 
     // ----------------------the non-flag arguments------------------------
+    // decided if header printing for directory is needed (either no dir argument or just one)
+    if(optind >=argc-1)
+        omitHeaderFlag=1;
+
     // no directory argument was passed in: print current directory
-    if (optind == argc)
-        print_directory_tree(".");
+    if (optind == argc){
+        omitHeaderFlag=1;
+        if ((ret = print_directory_tree(".") )!= 0) {
+            printf("UnixLs: cannot access '.': %s",strerror(ret));
+        }
+    }
     else {
         // Note: ls cmd always print . first, then .., then ~, (if they exist) then everything else
         // for this reason we first search for these three special cases
-        if (findArgument(".", argv, optind, argc - 1) > 0)  // even tho 0 indicates a found, argv[0] is program name
-            print_directory_tree(".");
-        if (findArgument("..", argv, optind, argc - 1) > 0)  // even tho 0 indicates a found, argv[0] is program name
-            print_directory_tree("..");
-        if (findArgument("~", argv, optind, argc - 1) > 0)  // even tho 0 indicates a found, argv[0] is program name
-            print_directory_tree("~");
+        if (findArgument(".", argv, optind, argc - 1) > 0){  // even tho 0 indicates a found, argv[0] is program name
+            if ((ret = print_directory_tree(".") )!= 0) {
+                printf("UnixLs: cannot access '.': %s",strerror(ret));
+            }
+        }
+        if (findArgument("..", argv, optind, argc - 1) > 0) {  // even tho 0 indicates a found, argv[0] is program name
+            if ((ret = print_directory_tree("..") )!= 0) {
+                printf("UnixLs: cannot access '..': %s",strerror(ret));
+            }
+        }
+        if (findArgument("~", argv, optind, argc - 1) > 0) {  // even tho 0 indicates a found, argv[0] is program name
+            if ((ret = print_directory_tree("~")) != 0) {
+                printf("UnixLs: cannot access '~': %s",strerror(ret));
+            }
+        }
 
         // print the rest of the directories
         for (index = optind; index < argc; index++) {
-            if (strcmp(argv[index], ".") && strcmp(argv[index], "~")) {
-                if ((ret = print_directory_tree(argv[index])) != 0)
-#ifdef DEBUG
-                    fprintf(stderr, "Printing directory '%s' error: %d", argv[index], ret);
-#endif
+            if (strcmp(argv[index], ".") && strcmp(argv[index], "..") && strcmp(argv[index], "~")) {
+                if ((ret = print_directory_tree(argv[index])) != 0){
+                    printf("UnixLs: cannot access '%s': %s",argv[index], strerror(ret));
+                }
             }
         }
     }
@@ -189,18 +208,27 @@ static int findArgument(char *pattern, char **strArr, int beginArrPos, int endAr
 static int printFile(const char *filepath, const struct stat *info,
                      const int typeflag, struct FTW *pathinfo) {
     // get size of file/dir
-    const double bytes;
+    unsigned int bytes;
     struct tm mtime;
     struct passwd *pwd;
     struct group *grp;
     char filename[BUFFSIZE];
+    char dateStrBuff[13];
+    int ret;
+
 
     // trim filepath
     getFilename(filename, filepath);
 
     // ignore . and .. and hidden files
-    if (isSpecialDir(filename) || isSubDir(filepath))
-        return 0;
+    ret=isSpecialDir(filename);
+    if (ret==1)
+        return FTW_CONTINUE;
+    else if(ret==2){     // if hidden dir, skip files
+        if (typeflag == FTW_D || typeflag == FTW_DP)
+            return FTW_SKIP_SUBTREE;
+        else return FTW_CONTINUE;
+    }
 
     // -i: begin the line with printing the inode #
     if (iFlag) {
@@ -216,7 +244,7 @@ static int printFile(const char *filepath, const struct stat *info,
         else
             printf("-");
         printf("%-10.10s", sperm(info->st_mode));
-        printf("%-4lu", info->st_nlink);
+        printf("%-2lu", info->st_nlink);
 
         // Print out owner's and group's name if it is found using getpwuid()
         // http://pubs.opengroup.org/onlinepubs/009695399/functions/getpwuid.html
@@ -232,7 +260,7 @@ static int printFile(const char *filepath, const struct stat *info,
 
 
         // print out file/dir size
-        bytes == (double) info->st_size; /* Not exact if large! */
+        bytes = (unsigned int) info->st_size; /* Not exact if large! */
 //        if (bytes >= 1099511627776.0)
 //            printf(" %9.3f TiB", bytes / 1099511627776.0);
 //        else if (bytes >= 1073741824.0)
@@ -242,16 +270,18 @@ static int printFile(const char *filepath, const struct stat *info,
 //        else if (bytes >= 1024.0)
 //            printf(" %9.3f KiB", bytes / 1024.0);
 //        else
-        printf(" %9.0f ", bytes);
+        printf(" %-6u", bytes);
 
         // get last modified timestamp of file/dir
         localtime_r(&(info->st_mtime), &mtime);
-        printf("%04d-%02d-%02d %02d:%02d:%02d",
-               mtime.tm_year + 1900, mtime.tm_mon + 1, mtime.tm_mday,
-               mtime.tm_hour, mtime.tm_min, mtime.tm_sec);
+//        printf(" %04d-%02d-%02d %02d:%02d:%02d",
+//               mtime.tm_year + 1900, mtime.tm_mon + 1, mtime.tm_mday,
+//               mtime.tm_hour, mtime.tm_min, mtime.tm_sec);
+        strftime(dateStrBuff, 20, "%b %d %H:%M", &mtime);
+        printf(" %s ",dateStrBuff);
     }
 
-    // name printing
+    // file/dir name printing
     // handle symbolic link
     if (typeflag == FTW_SL) {
         if (lFlag) {
@@ -301,89 +331,46 @@ static int printFile(const char *filepath, const struct stat *info,
         printf("%s\n", filename);
     else if (typeflag == FTW_D || typeflag == FTW_DP) {      // valid dir (FTW_DP = FTW_DEPTH was specified in flags)
         printf("%s\n", filename);
-        // unreliable, handle this manually
-//        return FTW_SKIP_SUBTREE;
+        return FTW_SKIP_SUBTREE;
     } else if (typeflag == FTW_DNR)       // unreadable dir
         printf("%s/ (unreadable)\n", filename);
     else
         printf("%s (unknown)\n", filename);
 
-    return 0;
+    return FTW_CONTINUE;
 }
 
-// obsolete
-//    // Declare a stat structure (e.g., buf), and then use this buf and dirPtr->d_name to call stat() or lstat() as appropriate.
-//    struct stat statBuf;
-//    errno = 0;
-//
-//    // lstat() retrieves the info about soft link instead of info about the file soft link is pointing to
-//    if (lstat(direntPtr->d_name, &statBuf) == -1) {
-//#ifdef DEBUG
-//        fprintf(stderr, "stat: cannot access '%s'", direntPtr->d_name);
-//        perror("");
-//#endif
-//        return;
-//    }
-//    // -i: begin the line with printing the inode #
-//    if (iFlag) {
-//        printf("%-7lu", statBuf.st_ino);    // unsigned long st_into
-//    }
-//
-//    // print out name of dir/file
-//    printf("%s\n",direntPtr->d_name);
-//
-////
-////    // -l: verbose mode printing all the details
-////    if (lFlag)
-////
-////        // Print out type, permissions, and number of links
-////        printf("%10.10s", sperm(statBuf.st_mode));
-////    printf("%4d", statBuf.st_nlink);
-////
-////
-////    /* Print out owner's name if it is found using getpwuid(). */
-////    if ((pwd = getpwuid(statBuf.st_uid)) != NULL)
-////        printf(" %-8.8s", pwd->pw_name);
-////    else
-////        printf(" %-8d", statBuf.st_uid);
-////
-////
-////    /* Print out group name if it is found using getgrgid(). */
-////    if ((grp = getgrgid(statBuf.st_gid)) != NULL)
-////        printf(" %-8.8s", grp->gr_name);
-////    else
-////        printf(" %-8d", statBuf.st_gid);
-////
-////
-////    /* Print size of file. */
-////    printf(" %9jd", (intmax_t) statBuf.st_size);
-////
-////
-////    tm = localtime(&statBuf.st_mtime);
-////
-////
-////    /* Get localized date string. */
-////    strftime(datestring, sizeof(datestring), nl_langinfo(D_T_FMT), tm);
-////
-////
-////    printf(" %s %s\n", datestring, dirPtr->d_name);
-
 // prints the directory passed in
-int print_directory_tree(const char *const dirpath) {
+// return errno (0= no error)
+int print_directory_tree(const char *dirpath) {
     int ret;
 
     // handle invalid directory path
     if (dirpath == NULL || *dirpath == '\0')
         return errno = EINVAL;
 
-    printf("%s:\n", dirpath);
-    strcpy(currDir, dirpath);
-    ret = nftw(dirpath, printFile, USE_FDS, FTW_PHYS);     /* set FTW_PHYS: Do not follow sym links
-*                                                                since we are doing it manually*/
-    puts("");
-    if (ret)
-        return ret;
+    if(!strcmp(dirpath,"~")){
+        // resolve special filepath '~'
+        // https://stackoverflow.com/a/2910392
+        struct passwd *pw = getpwuid(getuid());
+        dirpath = pw->pw_dir;
+    }
 
+    // no directory argument or just one
+    if(omitHeaderFlag)
+        omitHeaderFlag=0;       // reset in case -R is set
+    else
+        printf("%s:\n", dirpath);
+
+
+    strcpy(currDir, dirpath);
+    ret = nftw(dirpath, printFile, USE_FDS, FTW_PHYS | FTW_ACTIONRETVAL);     /* set FTW_PHYS: Do not follow sym links
+*                                                                since we are doing it manually*/
+    if (ret==-1){
+        errno=ENOENT;
+        return ENOENT;
+    }
+    puts("");
     // additionally, handle -R flag
     if (RFlag) {
         DIR *dir;
@@ -394,8 +381,9 @@ int print_directory_tree(const char *const dirpath) {
         if ((dir = opendir(dirpath)) == NULL) {
             ret = errno;
             fprintf(stderr, "Cannot open '%s': %s\n", dirpath, strerror(ret));
-            return ret;
+            return 0;
         }
+        strcpy(currDir, dirpath);
         while ((direntPtr = readdir(dir)) != NULL) {
             if (direntPtr->d_type == DT_DIR && !isSpecialDir(direntPtr->d_name)) {
                 strcpy(filepathBuff, dirpath);
@@ -404,13 +392,11 @@ int print_directory_tree(const char *const dirpath) {
                 if ((subDir = opendir(filepathBuff)) == NULL) {
                     ret = errno;
                     fprintf(stderr, "Cannot open '%s': %s\n", direntPtr->d_name, strerror(ret));
-                    return ret;
+                    return 0;
                 }
                 // print entries in subdir
                 if ((ret = print_directory_tree(filepathBuff)) != 0) {
-#ifdef DEBUG
-                    fprintf(stderr, "Error in print_directory_tree(\"%s\"): %d\n", direntPtr->d_name, ret);
-#endif
+                    printf("UnixLs: cannot access '%s': %s",filepathBuff,strerror(ret));
                 }
                 closedir(subDir);
             }
@@ -419,48 +405,6 @@ int print_directory_tree(const char *const dirpath) {
     }
     return 0;
 }
-
-/*
-// obsolete
-//// prints the all the file/dir info in a dir given
-//// recursive call to itself to handle the '-R' flag
-//// modified based on: https://stackoverflow.com/a/8438663
-//static void printDir(char *dirName) {
-//    DIR *dir;
-//    struct dirent *direntPtr;
-//
-//    // 1. Open a directory using opendir().
-//    errno = 0;
-//    if ((dir = opendir(dirName)) == NULL) {
-//        fprintf(stderr, "ls: cannot access '%s'", dirName);
-//        perror("");
-//        return;
-//    }
-//
-//    // print the current directories
-//    // control logic based on stat manual example here:
-//    // http://pubs.opengroup.org/onlinepubs/009695399/functions/stat.html
-//    errno=0;
-//    while ((direntPtr = readdir(dir)) != NULL) {
-//        // print file/dir's information if not (hidden || is .. || is .)
-//        if(direntPtr->d_type == DT_DIR && (direntPtr->d_name)[0]!= '.')
-//            printFile(direntPtr);
-//    }
-//
-//    if (errno != 0){
-//#ifdef DEBUG
-//        fprintf(stderr,"error reading directory '%s'",dirName);
-//        perror("");
-//#endif
-//        closedir(dir);
-//        return;
-//    }
-//
-//
-//    // 5. closedir(), once you have finished reading a directory it needs to be closed using closedir().
-//    closedir(dir);
-//}
- */
 
 // This is a non-standard system function on Solaris only
 // Hence must be defined in UNIX (despite having to type out its name..)
@@ -505,19 +449,24 @@ char const *sperm(__mode_t mode) {
 // Boolean func to determine whether the filepath is a special directory
 // special directory is defined as one of the three: '.', '..', or a hidden file (name beginning with .xx)
 // filename must not contain path (including ./xxx)
-// return 1 for true; 0 for false
+// return 1 for "." or ".."; return 2 for hidden file/dir; 0 for false
 int isSpecialDir(const char *filename) {
-    if (strlen(filename) < 1 || filename[0] == '.')        // either . or ..
+    if(!strcmp(filename,".") || !strcmp(filename,"."))
         return 1;
+    else if(strlen(filename) && filename[0]=='.'){
+        // hidden file/dir
+        return 2;
+    }
     else return 0;
 }
 
+#ifdef DEBUG
 // determines if the first x chars matches currDir, if it does, we can assume that its a subdirectory
 // handles filepaths in the format of both './xxx' and plain 'xxx'
-// returns non-zero for true, 0 for false
-int isSubDir(const char *filePath) {
+// returns ptr to the location of '/' for true, 0 for false
+char *isSubDir(const char *filePath) {
     char *ptr = (char *) filePath;
-    int len = strlen(currDir);
+    size_t len = strlen(currDir);
     // if filepath has the format of 'currDir/xxx', we trim currDir/ from filePath
     if (strlen(filePath) > len) {
         char strBuff[BUFFSIZE];
@@ -529,16 +478,18 @@ int isSubDir(const char *filePath) {
                 ptr++;
         }
     }
-    return (int) strchr(ptr, '/');
+    return strchr(ptr, '/');
 }
+#endif
 
 // this function trims the filepath
 // similar to basename(), but substitutes the filename to '.' if it matches currDir[]
 void getFilename(char *filename, const char *filePath) {
     if (!strcmp(filePath, currDir)) {
         filename[0] = '.';
+        filename[1]='\0';
     } else {
-        char *namePtr = basename(filePath);
+        char *namePtr = basename((char *) filePath);        // basename discards const qualifier
         strcpy(filename, namePtr);
     }
 
